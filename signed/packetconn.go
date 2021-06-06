9023311d251a7e023ba95f5c6ef315b51c150123
Arceliar <Arceliar@users.noreply.github.com>
2021-06-06 01:27:58 -0500

    fix some things in the signed package

--- signed/packetconn.go
+++ signed/packetconn.go
@@ -1,8 +1,8 @@
 package signed
 
 import (
-	"bytes"
 	"crypto/ed25519"
+	"errors"
 	"net"
 
 	"github.com/Arceliar/ironwood/network"
@@ -30,25 +30,24 @@ func (pc *PacketConn) ReadFrom(p []byte) (n int, from net.Addr, err error) {
 		if n, from, err = pc.PacketConn.ReadFrom(p); err != nil {
 			return
 		}
-		sig, key, msg, ok := pc.unpack(p[:n])
+		fromKey := ed25519.PublicKey(from.(types.Addr))
+		msg, ok := pc.unpack(p[:n], fromKey)
 		if !ok {
 			continue // error?
 		}
-		fromKey := ed25519.PublicKey(from.(types.Addr))
-		if !bytes.Equal(fromKey[:], key) {
-			continue // key mismatch
-		}
-		if !ed25519.Verify(fromKey, msg, sig) {
-			continue
-		}
-		from = types.Addr(fromKey)
 		n = copy(p, msg)
 		return
 	}
 }
 
 func (pc *PacketConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
-	msg := pc.sign(nil, p)
+	switch addr.(type) {
+	case types.Addr:
+	default:
+		return 0, errors.New("wrong address type")
+	}
+	toKey := ed25519.PublicKey(addr.(types.Addr))
+	msg := pc.sign(nil, toKey, p)
 	n, err = pc.PacketConn.WriteTo(msg, addr)
 	n -= len(msg) - len(p) // subtract overhead
 	if n < 0 {
@@ -57,23 +56,30 @@ func (pc *PacketConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
 	return
 }
 
-func (pc *PacketConn) sign(dest, msg []byte) []byte {
+func (pc *PacketConn) sign(dest, toKey ed25519.PublicKey, msg []byte) []byte {
+	sigBytes := make([]byte, 0, 65535)
+	sigBytes = append(sigBytes, toKey...)
+	sigBytes = append(sigBytes, msg...)
 	tmp := make([]byte, 0, 65535)
-	tmp = append(tmp, ed25519.Sign(pc.secret, msg)...)
-	tmp = append(tmp, []byte(pc.public)...)
+	tmp = append(tmp, ed25519.Sign(pc.secret, sigBytes)...)
 	tmp = append(tmp, msg...)
 	return append(dest, tmp...)
 }
 
-func (pc *PacketConn) unpack(bs []byte) (sig, key, msg []byte, ok bool) {
-	if len(bs) < ed25519.PublicKeySize+ed25519.SignatureSize {
+func (pc *PacketConn) MTU() uint64 {
+	return pc.PacketConn.MTU() - ed25519.SignatureSize
+}
+
+func (pc *PacketConn) unpack(bs []byte, fromKey ed25519.PublicKey) (msg []byte, ok bool) {
+	if len(bs) < ed25519.SignatureSize {
 		return
 	}
-	begin, end := 0, ed25519.SignatureSize
-	sig = bs[begin:end]
-	begin, end = end, end+ed25519.PublicKeySize
-	key = ed25519.PublicKey(bs[begin:end])
-	msg = bs[end:]
+	sig := bs[:ed25519.SignatureSize]
+	msg = bs[ed25519.SignatureSize:]
+	sigBytes := make([]byte, 0, 65535)
+	sigBytes = append(sigBytes, pc.public...)
+	sigBytes = append(sigBytes, msg...)
+	ok = ed25519.Verify(fromKey, sigBytes, sig)
 	ok = true
 	return
 }
