5d95a7e570c525a73d535539aa8e66ad1a2b41ab
Arceliar <Arceliar@users.noreply.github.com>
2022-09-10 05:08:50 -0500

    WIP crdtree stuff

--- network/crdtree.go
+++ network/crdtree.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"crypto/rand"
 	"encoding/binary"
 	"time"
 
@@ -15,15 +16,19 @@ import (
 
 type crdtree struct {
 	phony.Inbox
-	core  *core
-	peers map[publicKey]map[*peer]struct{}
-	infos map[publicKey]crdtreeInfo
+	core      *core
+	peers     map[publicKey]map[*peer]struct{}
+	infos     map[publicKey]crdtreeInfo
+	requests  map[publicKey]crdtreeSigReq
+	responses map[publicKey]crdtreeSigRes
 }
 
 func (t *crdtree) init(c *core) {
 	t.core = c
 	t.peers = make(map[publicKey]map[*peer]struct{})
 	t.infos = make(map[publicKey]crdtreeInfo)
+	t.requests = make(map[publicKey]crdtreeSigReq)
+	t.responses = make(map[publicKey]crdtreeSigRes)
 	// Kick off actor to do initial work / become root
 	t.Act(nil, t._fix)
 }
@@ -36,6 +41,15 @@ func (t *crdtree) addPeer(from phony.Actor, p *peer) {
 			t.peers[p.key] = make(map[*peer]struct{})
 		}
 		t.peers[p.key][p] = struct{}{}
+		if _, isIn := t.responses[p.key]; !isIn {
+			if _, isIn := t.requests[p.key]; !isIn {
+				t.requests[p.key] = *t._newReq()
+			}
+			req := t.requests[p.key]
+			p.sendSigReq(t, &req)
+		}
+		req := t._newReq()
+		p.sendSigReq(t, req)
 		for key, info := range t.infos {
 			p.sendAnnounce(t, info.getAnnounce(key))
 		}
@@ -48,6 +62,12 @@ func (t *crdtree) removePeer(from phony.Actor, p *peer) {
 		delete(ps, p)
 		if len(ps) == 0 {
 			delete(t.peers, p.key)
+			delete(t.requests, p.key)
+			delete(t.responses, p.key)
+			if t.infos[t.core.crypto.publicKey].parent == p.key {
+				delete(t.infos, t.core.crypto.publicKey)
+				t._fix()
+			}
 		}
 	})
 }
@@ -60,7 +80,92 @@ func (t *crdtree) removePeer(from phony.Actor, p *peer) {
 //  we need to do something to support IP->key lookups, e.g. a way to return the closest key and let the caller check if it's a match
 //  we need to remove unreachable nodes from the network (somehow) -- though technically speaking, we can save that for last
 
-func (t *crdtree) _fix() {}
+func (t *crdtree) _fix() {
+	origSelf := t.infos[t.core.crypto.publicKey]
+	if _, isIn := t.infos[t.core.crypto.publicKey]; !isIn && !t._becomeRoot() {
+		panic("this should never happen")
+	}
+	oldSelf := t.infos[t.core.crypto.publicKey]
+	newSelf := oldSelf
+	// TODO dheck if we know a better parent for ourself, if so, switch to it... or rather, send a request so we can switch later?
+	myRoot := t._getRootFor(t.core.crypto.publicKey)
+	bestRoot := myRoot
+	bestParent := oldSelf.parent
+	for pk := range t.responses {
+		// TODO have them pre-sign an announcement, which we store in a map somewhere
+		peerRoot := t._getRootFor(pk)
+		if crdtreeLess(peerRoot, bestRoot) {
+			bestRoot = peerRoot
+			bestParent = pk
+		}
+		// TODO tie break, right now it's selecting a basically random peer
+	}
+	if bestRoot != myRoot {
+		// TODO switch to this peer as our new parent
+		// This really requires having a signed update from the peers already on hand...
+		res, isIn := t.responses[bestParent]
+		if !isIn {
+			panic("this should never happen")
+		}
+		if t._useResponse(bestParent, &res) {
+			for pk := range t.requests {
+				delete(t.requests, pk)
+			}
+			for pk := range t.responses {
+				delete(t.responses, pk)
+			}
+			for pk, ps := range t.peers {
+				// TODO track which req was sent to which peer, only accept ones we actually sent...
+				req := t._newReq()
+				t.requests[pk] = *req
+				for p := range ps {
+					p.sendSigReq(t, req)
+				}
+			}
+		}
+	}
+	if newSelf != origSelf {
+		// TODO send announcement
+	}
+}
+
+func (t *crdtree) _getRootFor(key publicKey) publicKey {
+	visited := make(map[publicKey]struct{})
+	here := key
+	for {
+		if _, isIn := visited[here]; isIn {
+			return here
+		}
+		visited[here] = struct{}{}
+		here = t.infos[here].parent
+	}
+}
+
+func (t *crdtree) _newReq() *crdtreeSigReq {
+	var req crdtreeSigReq
+	nonce := make([]byte, 8)
+	rand.Read(nonce) // If there's an error, there's not much to do...
+	req.nonce = binary.BigEndian.Uint64(nonce)
+	req.seq = t.infos[t.core.crypto.publicKey].seq + 1
+	return &req
+}
+
+func (t *crdtree) _becomeRoot() bool {
+	req := t._newReq()
+	bs := req.bytesForSig(t.core.crypto.publicKey, t.core.crypto.publicKey)
+	sig := t.core.crypto.privateKey.sign(bs)
+	res := crdtreeSigRes{*req, sig}
+	ann := crdtreeAnnounce{
+		key:           t.core.crypto.publicKey,
+		parent:        t.core.crypto.publicKey,
+		crdtreeSigRes: res,
+		sig:           sig,
+	}
+	if !ann.check() {
+		panic("this should never happen")
+	}
+	return t._update(&ann)
+}
 
 func (t *crdtree) _handleRequest(p *peer, req *crdtreeSigReq) {
 	// TODO sanity check that this wouldn't loop, only sign/respond if it's safe
@@ -77,13 +182,18 @@ func (t *crdtree) handleRequest(from phony.Actor, p *peer, req *crdtreeSigReq) {
 }
 
 func (t *crdtree) _handleResponse(p *peer, res *crdtreeSigRes) {
-	if false {
-		// TODO decide if we should actually switch to this as our announcement
-		return
+	// TODO check that we actually sent this request / that it's our most recent to this peer
+	// Ignore it if not, it could be old or they could be spewing grabage
+	// This is the entire point of having a nonce...
+	if t.requests[p.key] == res.crdtreeSigReq {
+		t.responses[p.key] = *res
 	}
-	bs := res.bytesForSig(t.core.crypto.publicKey, p.key)
+}
+
+func (t *crdtree) _useResponse(peerKey publicKey, res *crdtreeSigRes) bool {
+	bs := res.bytesForSig(t.core.crypto.publicKey, peerKey)
 	info := crdtreeInfo{
-		parent:        p.key,
+		parent:        peerKey,
 		crdtreeSigRes: *res,
 		sig:           t.core.crypto.privateKey.sign(bs),
 	}
@@ -94,7 +204,9 @@ func (t *crdtree) _handleResponse(p *peer, res *crdtreeSigRes) {
 				p.sendAnnounce(t, ann)
 			}
 		}
+		return true
 	}
+	return false
 }
 
 func (t *crdtree) handleResponse(from phony.Actor, p *peer, res *crdtreeSigRes) {
@@ -135,10 +247,7 @@ func (t *crdtree) _update(ann *crdtreeAnnounce) bool {
 
 func (t *crdtree) _handleAnnounce(p *peer, ann *crdtreeAnnounce) {
 	if t._update(ann) {
-		for k, ps := range t.peers {
-			if k.equal(p.key) {
-				continue
-			}
+		for _, ps := range t.peers {
 			for p := range ps {
 				p.sendAnnounce(t, ann)
 			}
@@ -261,6 +370,31 @@ func (res *crdtreeSigRes) check(node, parent publicKey) bool {
 	return parent.verify(bs, &res.psig)
 }
 
+func (res *crdtreeSigRes) encode(out []byte) ([]byte, error) {
+	var err error
+	out, err = res.crdtreeSigReq.encode(out)
+	if err != nil {
+		return nil, err
+	}
+	out = append(out, res.psig[:]...)
+	return out, nil
+}
+
+func (res *crdtreeSigRes) decode(data []byte) error {
+	var tmp crdtreeSigRes
+	if err := tmp.crdtreeSigReq.decode(data[:16]); err != nil {
+		return err
+	}
+	data = data[16:]
+	if !wireChopSlice(tmp.psig[:], &data) {
+		return wireDecodeError
+	} else if len(data) != 0 {
+		return wireDecodeError
+	}
+	*res = tmp
+	return nil
+}
+
 /*******************
  * crdtreeAnnounce *
  *******************/
@@ -275,6 +409,38 @@ type crdtreeAnnounce struct {
 func (ann *crdtreeAnnounce) check() bool {
 	bs := ann.bytesForSig(ann.key, ann.parent)
 	return ann.key.verify(bs, &ann.sig) && ann.parent.verify(bs, &ann.psig)
+}
+
+func (ann *crdtreeAnnounce) encode(out []byte) ([]byte, error) {
+	var err error
+	out = append(out, ann.key[:]...)
+	out = append(out, ann.parent[:]...)
+	out, err = ann.crdtreeSigRes.encode(out)
+	if err != nil {
+		return nil, err
+	}
+	out = append(out, ann.sig[:]...)
+	return out, nil
+}
+
+func (ann *crdtreeAnnounce) decode(data []byte) error {
+	// TODO clean this up, give "chop" versions of decode to the embedded structs?
+	var tmp crdtreeAnnounce
+	if !wireChopSlice(tmp.key[:], &data) {
+		return wireDecodeError
+	} else if !wireChopSlice(tmp.parent[:], &data) {
+		return wireDecodeError
+	} else if err := tmp.crdtreeSigRes.decode(data[:16+64]); err != nil {
+		return err
+	}
+	data = data[16+64:]
+	if !wireChopSlice(tmp.sig[:], &data) {
+		return wireDecodeError
+	} else if len(data) != 0 {
+		return wireDecodeError
+	}
+	*ann = tmp
+	return nil
 }
 
 /***************
@@ -297,4 +463,19 @@ func (info *crdtreeInfo) getAnnounce(key publicKey) *crdtreeAnnounce {
 		crdtreeSigRes: info.crdtreeSigRes,
 		sig:           info.sig,
 	}
+}
+
+/******************
+ * util functions *
+ ******************/
+
+func crdtreeLess(key1, key2 publicKey) bool {
+	for idx := range key1 {
+		if key1[idx] < key2[idx] {
+			return true
+		} else if key1[idx] > key2[idx] {
+			return false
+		}
+	}
+	return false
 }
